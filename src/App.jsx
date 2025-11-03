aebc8958e79b2cc5bb9f4a4ae45a71d986f744ff
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-11-03 15:39:51 +0000

    react + tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-4xl font-bold">
+      Tailwind v4 + Vite + React 🚀
+    </div>
+  );
+}
+
