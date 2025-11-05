97a660a9ab767dabdc9c534c15a18beb6293bfcd
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-11-05 15:06:53 +0000

    tweek: in header on large screen

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import "../styles/nav.css";
 import { useState } from "react";
+import profileIcon from "../images/profile-icon.png";
 export default function Nav() {
   const [openClose, setOpenClose] = useState(false);
   function toggleBtn() {
     return setOpenClose((btn) => !btn);
   }
   return (
-    <div className="relative">
-      {/* Navigation button */}
+    <nav className="relative flex justify-between items-center w-full px-5">
+      {/* hamburger button */}
       <button
         id="menu-btn"
         onClick={toggleBtn}
@@ -23,12 +24,12 @@ export default function Nav() {
       </button>
       {/* Dropdown navigation */}
       <div
-        className={`h-screen fixed top-[100px]  w-full ${
+        className={`h-screen fixed top-[100px]  w-full lg:flex ${
           openClose ? "block" : "hidden"
-        }`}
+        } lg:static lg:h-auto lg:w-auto lg:top-auto lg:order-2`}
       >
-        <ul className="bg-gray-200 w-full flex flex-col items-center p-10 h-screen space-y-5 ">
-          <li className="flex space-x-5">
+        <ul className="bg-gray-200 w-full flex flex-col items-center p-10 h-screen space-y-5 lg:space-y-0 lg:space-x-9 lg:flex-row lg:bg-transparent lg:h-auto">
+          <li className="flex space-x-5 lg:space-y-0 lg-p-0 lg:hidden">
             <section>
               <Link to="#">Log in</Link>
             </section>
@@ -53,6 +54,10 @@ export default function Nav() {
           </li>
         </ul>
       </div>
-    </div>
+      <h1 className="text-2xl lg:order-1">CookBakeShare</h1>
+      <div className="m-3 lg:order-3">
+        <img src={profileIcon} alt="user profile icon" className="w-12" />
+      </div>
+    </nav>
   );
 }
