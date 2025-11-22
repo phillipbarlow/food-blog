1bc105f24747761e0c7997459371cc3325baf8aa
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-11-22 18:30:10 +0000

    auth context initiated

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,20 @@
+import { useState} from "react";
+import AuthContext  from "./AuthContext";
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const login = (email) => {
+    // In real life you’d call an API here and get user data + token
+    setUser({
+      name: "Phil",
+      email: email,
+    });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+  const value = { user, login, logout };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
