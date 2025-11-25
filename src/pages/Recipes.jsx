1520232a58b025cc17ed3ac1b185f8b8ef84ee6f
phillipbarlow10@gmail.com <phillipbarlow10@gmail.com>
2025-11-25 11:17:31 +0000

    tidy up

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+export default function Recipes() {
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch("http://localhost:5001/recipes");
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = res.json();
+        console.log("Recipes: ", data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+    fetchRecipes();
+  }, []);
 
-export default function Recipes(){
-    return(
-        <div>
-            
-        </div>
-    )
+  return <div></div>;
 }
